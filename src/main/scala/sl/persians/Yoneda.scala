2a4b1af794a106999da861ea14fddefa5f3c3abd
Stephen Lazaro <stephen@earnest.com>
2017-01-29 21:44:03 -0800

    Applicative syntax and instance

--- src/main/scala/sl/persians/Yoneda.scala
+++ src/main/scala/sl/persians/Yoneda.scala
@@ -1,6 +1,6 @@
 package sl.persians
 
-import cats.{Apply, Functor}
+import cats.{Applicative, Apply, Functor}
 import cats.syntax.apply._
 import shapeless.PolyDefns.~>
 
@@ -21,7 +21,7 @@ import shapeless.PolyDefns.~>
   * which amounts to showing that liftYoneda and
   * lowerYoneda really do work, in other words
   * that values are determined by lifting the identity
-  * across our natural transformation.
+  * across each given natural transformation.
   */
 trait Yoneda [F [_], A] {
   def roYo: λ [B => (A => B)] ~> F
@@ -36,14 +36,14 @@ object Yoneda {
   def upYo [F [_] : Functor, A] (fa: F [A]) = liftYoneda [F, A] (fa)
   def yUp  [F [_] : Functor, A] (fa: F [A]) = liftYoneda [F, A] (fa)
 
-  def lowerYoneda [F [_]: Functor, A] (ya: Yoneda [F, A]): F [A] =
+  def lowerYoneda [F [_], A] (ya: Yoneda [F, A]): F [A] =
     ya roYo identity [A] _
   // Apparently this can't be eta reduced, though I am not sure why
-  def loYo [F [_] : Functor, A] (ya: Yoneda [F, A]) = lowerYoneda [F, A] (ya)
-  def yoLo [F [_] : Functor, A] (ya: Yoneda [F, A]) = lowerYoneda [F, A] (ya)
+  def loYo [F [_], A] (ya: Yoneda [F, A]) = lowerYoneda [F, A] (ya)
+  def yoLo [F [_], A] (ya: Yoneda [F, A]) = lowerYoneda [F, A] (ya)
 
   object instances {
-    implicit def stdFunctorForYoneda [F [_]] = new Functor [Yoneda [F, ?]] {
+    implicit def persiansStdFunctorForYoneda [F [_]] = new Functor [Yoneda [F, ?]] {
       def map [A, B] (fa: Yoneda [F, A])(f: A => B) =
         new Yoneda [F, B] {
           def roYo = new (λ [C => (B => C)] ~> F) {
@@ -51,20 +51,42 @@ object Yoneda {
           }
         }
     }
-    implicit class functorSyntaxForYoneda [F [_], A] (ya: Yoneda [F, A]) {
-      def map [B] (f: A => B) = stdFunctorForYoneda.map [A, B] (ya)(f)
-    }
 
-    implicit def stdApplyForYoneda [F [_] : Apply] = new Apply [Yoneda [F, ?]] {
-      def map [A, B] (fa: Yoneda [F, A])(f: A => B) = stdFunctorForYoneda.map(fa)(f)
-      def ap [A, B] (ff: Yoneda [F, A => B])(ya: Yoneda [F, A]): Yoneda [F, B] =
+    implicit def persiansStdApplyForYoneda [F [_] : Apply] = new Apply [Yoneda [F, ?]] {
+      import syntax.functorSyntaxForYoneda
+      def map [A, B] (ya: Yoneda [F, A])(f: A => B) = persiansStdFunctorForYoneda .map (ya) (f)
+      def ap [A, B] (yf: Yoneda [F, A => B])(ya: Yoneda [F, A]): Yoneda [F, B] =
         new Yoneda [F, B] {
           def roYo = new (λ [C => (B => C)] ~> F) {
             def apply [T] (k: B => T) =
-              (Yoneda loYo [F, A => T] (ff map (k compose _))) ap (
-                Yoneda loYo [F, A] ya)
+              (Yoneda loYo (yf map (k compose _))) ap (
+                Yoneda loYo  ya)
           }
         }
+    }
+
+    implicit def persiansStdApplicativeForYoneda [F [_] : Applicative] = new Applicative [Yoneda [F, ?]] {
+      import syntax.applySyntaxForYoneda
+      def ap [A, B] (yf: Yoneda [F, A => B])(ya: Yoneda [F, A]): Yoneda [F, B] = ya ap yf
+      def pure [A] (a: A) = Yoneda upYo (implicitly [Applicative [F]] pure a)
+    }
+  }
+
+  object syntax {
+    implicit class functorSyntaxForYoneda [F [_], A] (ya: Yoneda [F, A]) {
+      import instances.persiansStdFunctorForYoneda
+      def map [B] (f: A => B) = persiansStdFunctorForYoneda.map [A, B] (ya)(f)
+    }
+
+    implicit class applySyntaxForYoneda [F [_] : Apply, A] (ya: Yoneda [F, A]) {
+      import instances.persiansStdApplyForYoneda
+      def map [B] (f: A => B) = persiansStdApplyForYoneda [F] .map [A, B] (ya)(f)
+      def ap [B] (yf: Yoneda[F, A => B]) = persiansStdApplyForYoneda [F] .ap [A, B] (yf)(ya)
+    }
+
+    implicit class applicativeSyntaxForYoneda [F [_] : Applicative, A] (ya: Yoneda [F, A]) {
+      import instances.persiansStdApplicativeForYoneda
+      def ap [B] (yf: Yoneda [F, A => B]) = persiansStdApplicativeForYoneda [F] .ap [A, B] (yf)(ya)
     }
   }
 }
