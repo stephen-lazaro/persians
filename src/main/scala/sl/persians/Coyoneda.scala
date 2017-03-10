3c4c878e403920819b393b99dc76e70fd798a202
Stephen Lazaro <stephen@earnest.com>
2017-03-09 22:10:30 -0800

    Initial Coyoneda thoughts

--- src/main/scala/sl/persians/Coyoneda.scala
+++ src/main/scala/sl/persians/Coyoneda.scala
@@ -1,7 +1,16 @@
 package sl.persians
 
 import cats.Functor
-
+/**
+ * TBH I am having a great deal of difficulty
+ * really grokking this one. But I believe the deal
+ * is that we are holding a fixed value, and tracking
+ * various functional steps through the lattice of types
+ * by composing functions. Think of this as maybe a lazy
+ * sort of functional application by just composing all
+ * the relevant functions and running them all at once
+ * at the end of the universe.
+ */
 trait Coyoneda [F [_], A] {
   type Relevant
   def run: Relevant => A
