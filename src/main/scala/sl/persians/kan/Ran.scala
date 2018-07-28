74faa9cbfb21a81c56c2ffb96bd6a19d07e167e3
Stephen Lazaro <stephen.lazaro@gmail.com>
2018-07-27 17:35:47 -0700

    Some Ran for your Kan

--- src/main/scala/sl/persians/kan/Ran.scala
+++ src/main/scala/sl/persians/kan/Ran.scala
@@ -1,13 +1,16 @@
 package sl.persians.kan
 
-//import cats.{~>, Functor}
+import cats.{~>, Functor}
 
 trait Ran [G [_], H [_], A] {
   def run [B] (a: A => G [B]): H [B]
 }
 object Ran {
-  //def toRan [F [_]: Functor, H [_], G [_], B] (trans: H ~> λ[A => F [G [A]]]): Ran[G, H, B => F[B]] =
-   // new Ran [G, H, B => F[B]] {
-   //   def run [C] (given: A => G[B => F[C]]) = ???
-    //}
+  def toRan [F [_]: Functor, G [_], H [_], B] (trans: λ[A => F [G [A]]] ~> H)(fb: F[B]): Ran[G, H, B] =
+    new Ran [G, H, B] {
+      def run [C] (given: B => G[C]): H[C] = trans.apply[C](Functor[F].map(fb)(given))
+    }
+
+  def fromRan [F[_], G[_], H[_], B](trans: F ~> Ran[G, H, ?])(fgb: F[G[B]]): H[B] =
+    trans.apply[G[B]](fgb).run(identity[G[B]])
 }
