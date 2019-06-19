5bceae26b2c76dd37cc452b699665f86ba3bd977
Stephen Lazaro <stephen.lazaro@gmail.com>
2019-06-19 10:14:44 -0700

    Drastically improve ZModulo

--- src/main/scala/sl/persians/ZModulo.scala
+++ src/main/scala/sl/persians/ZModulo.scala
@@ -1,86 +1,90 @@
 package sl.persians
 
 import eu.timepit.refined._
-import eu.timepit.refined.api.{Refined, Validate}
-import eu.timepit.refined.auto._
-import eu.timepit.refined.numeric._
-import shapeless.{Nat, Widen, Witness}
-import shapeless.ops.nat.ToInt
+import eu.timepit.refined.api.Refined
+import eu.timepit.refined.numeric.Interval
+import shapeless.{Widen, Witness}
+import singleton.ops.{>=, Require}
 import spire.algebra.{CModule, CRing}
 
-case class ZModulo[Upper: Witness.Aux](value: Int Refined Interval.ClosedOpen[W.`0`.T,  Upper])
+final class ZModulo[Upper] private[persians] (val value: Int) extends AnyVal
 object ZModulo {
+  type Domain[Upper] = Interval.ClosedOpen[W.`0`.T,  Upper]
+
+  def fromRefined[Upper](r: Int Refined Domain[Upper]): ZModulo[Upper] =
+    new ZModulo[Upper](r.value)
+
+  def unapply[Upper](arg: ZModulo[Upper]): Option[Int] = Some(arg.value)
+
   implicit def module[Upper <: Int](
     implicit
     witness: Witness.Aux[Upper],
-    validate: Validate[Int, Interval.ClosedOpen[W.`0`.T,  Upper]],
-    widen: Widen.Aux[Upper, Int]
+    widen: Widen.Aux[Upper, Int],
+    requirement: Require[Upper >= W.`1`.T]
   ): CModule[ZModulo[Upper], Int] =
-    if (widen(witness.value) >= 1)
       new CModule[ZModulo[Upper], Int] {
         override implicit def scalar: CRing[Int] = CRing[Int]
 
         override def timesl(r: Int, v: ZModulo[Upper]): ZModulo[Upper] = v match {
-          case ZModulo(value) => ZModulo[Upper](Refined.unsafeApply((r * value.value) % widen(witness.value)))
+          case ZModulo(value) =>
+            new ZModulo[Upper]((r * value) % widen(witness.value))
         }
 
         override def negate(x: ZModulo[Upper]): ZModulo[Upper] = x match {
           case ZModulo(value) =>
-            ZModulo[Upper](Refined.unsafeApply((widen(witness.value) - value) % widen(witness.value)))
+            new ZModulo[Upper]((widen(witness.value) - value) % widen(witness.value))
         }
 
-        override val zero: ZModulo[Upper] = ZModulo[Upper](refineV(0).right.get)
+        override val zero: ZModulo[Upper] = new ZModulo[Upper](0)
 
         override def plus(x: ZModulo[Upper], y: ZModulo[Upper]): ZModulo[Upper] = (x, y) match {
           case (ZModulo(a), ZModulo(b)) =>
-            ZModulo[Upper](Refined.unsafeApply((a + b) % widen(witness.value)))
+            new ZModulo[Upper]((a + b) % widen(witness.value))
         }
       }
-    else throw new Exception("Modulus must be strictly positive")
 
   implicit def cring[Upper <: Int](
     implicit
     witness: Witness.Aux[Upper],
-    validate: Validate[Int, Interval.ClosedOpen[W.`0`.T,  Upper]],
     widen: Widen.Aux[Upper, Int],
+    requirement: Require[Upper >= W.`1`.T]
   ): CRing[ZModulo[Upper]] =
-    if (widen(witness.value) >= 1)
       new CRing[ZModulo[Upper]] {
-        override val one: ZModulo[Upper] = ZModulo[Upper](refineV(1).right.get)
+        override val one: ZModulo[Upper] = new ZModulo[Upper](1)
 
         override def plus(x: ZModulo[Upper], y: ZModulo[Upper]): ZModulo[Upper] =
-          ZModulo[Upper](
+          new ZModulo[Upper](
             (x, y) match {
               case (ZModulo(a), ZModulo(b)) =>
-                Refined.unsafeApply((a.value + b.value) % widen(witness.value))
+                (a + b) % widen(witness.value)
             }
           )
 
         override def times(x: ZModulo[Upper], y: ZModulo[Upper]): ZModulo[Upper] =
-          ZModulo[Upper](
+          new ZModulo[Upper](
             (x, y) match {
               case (ZModulo(a), ZModulo(b)) =>
-                Refined.unsafeApply((a.value * b.value) % widen(witness.value))
+                (a * b) % widen(witness.value)
             }
           )
 
         override def negate(x: ZModulo[Upper]): ZModulo[Upper] =
-          ZModulo[Upper](
+          new ZModulo[Upper](
             x match {
               case ZModulo(value) =>
-                Refined.unsafeApply((24 - value.value) % widen(witness.value))
+                (24 - value) % widen(witness.value)
             }
           )
 
-        override val zero: ZModulo[Upper] = ZModulo[Upper](refineV(0).right.get)
+        override val zero: ZModulo[Upper] = new ZModulo[Upper](0)
       }
-    else throw new Exception("Modulus must be strictly positive.")
 
+  // The only way to create ZModulo[Upper] is via the morphism from Int
   implicit def morphismFromInt[Upper <: Int](
     implicit
     witness: Witness.Aux[Upper],
-    validate: Validate[Int, Interval.ClosedOpen[W.`0`.T,  Upper]],
     widen: Widen.Aux[Upper, Int],
+    requirement: Require[Upper >= W.`1`.T]
   ): Surjection[Int, ZModulo[Upper]] = new Surjection[Int, ZModulo[Upper]] {
     // Summon these eagerly to force errors
     val cmod: CModule[ZModulo[Upper], Int] = CModule[ZModulo[Upper], Int]
@@ -90,6 +94,6 @@ object ZModulo {
       cmod.timesl(a, cring.one)
 
     override def section(b: ZModulo[Upper]): Int =
-      b.value.value
+      b.value
   }
 }
